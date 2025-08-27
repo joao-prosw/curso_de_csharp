223b525b1f88724a8e5652288576b954c983150c
joao-prosw <joaopaulomsantos11@gmail.com>
2025-08-26 23:43:32 -0300

    Exemplo 3

---
+++ Exemplo_3/Models/Calculadora.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exemplo_3.Models
+{
+    public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"Resultado: {x} + {y} = {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"Resultado: {x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"Resultado: {x} * {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"Resultado: {x} / {y} = {x / y}");
+        }
+
+        public void Potencia(int x, int y)
+        {
+            double potencia = Math.Pow(x, y);
+            Console.WriteLine($"Resultado: {x} ^ {y} = {potencia}");
+        }
+
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            Console.WriteLine($"Seno de {angulo} = {Math.Round(Math.Sin(radiano), 4)}");
+        }
+
+        public void Coseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            Console.WriteLine($"Coseno de {angulo} = {Math.Round(Math.Cos(radiano), 4)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            Console.WriteLine($"Tangente de {angulo} = {Math.Round(Math.Tan(radiano), 4)}");
+        }
+
+        public void RaizQuadrada(double x)
+        {
+            Console.WriteLine($"Raíz quadrada de {x} = {Math.Sqrt(x)}");
+        }
+    }
+}
---
+++ Exemplo_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exemplo_3.Models;
+using System;
+
+Calculadora Calc = new Calculadora();
+
+Calc.Somar(5, 7);
+Calc.Subtrair(10, 7);
+Calc.Multiplicar(2, 5);
+Calc.Dividir(8, 4);
+Calc.Potencia(3, 2);
+Calc.Seno(30);
+Calc.Coseno(30);
+Calc.Tangente(30);
+Calc.RaizQuadrada(9);
+
+int num = 10;
+num++;
+Console.WriteLine(num);
+num--;
+Console.WriteLine(num);
